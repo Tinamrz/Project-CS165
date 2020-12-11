ac4b23f7d2f667fa8fc1cd45b8d48b156a36238a
Tinamrz <41027188+Tinamrz@users.noreply.github.com>
2020-12-10 22:01:19 -0800

    Update proxy.c

--- src/proxy.c
+++ src/proxy.c
@@ -13,6 +13,8 @@
 #include <tls.h>
 #include <unistd.h>
 #include "murmur3.h"
+#include "bloom.h"
+
 //your application name -port portnumber -servername:serverportnumber
 static void usage()
 {
@@ -66,12 +68,37 @@ int main(int argc,  char *argv[])
 	uint32_t seed = 42;              // Seed value for hash
 	MurmurHash3_x86_32(strcat(argv[4],proxyname), strlen(strcat(argv[4],proxyname)), seed, hash);
 	
+	// For the bloom filter we are using the bloom filter library
+	bloom_parameters parameters;
+	parameters.projected_element_count = 30000;  //objects
+	parameters.false_positive_probability = 0.01;   //probability of the false positive
+	// Simple randomizer 
+   	parameters.random_seed = 0xA5A5A5A5;
+	if (!parameters)
+   	{
+      	std::cout << "Error - Invalid set of bloom filter parameters!" << std::endl;
+      	return 1;
+   	}
+	bloom_filter filter(parameters);
 	
-	// Kramer: Similarly to the server code, we need to set up a tls. The proxy acts as a middle-man in-between 
-	// the server and the client, acting as a "server of the server" if you will.
-	
-	struct tls_config *serverConfigTLS = tls_config_new();
-	struct tls_config *clientConfigTLS = NULL;
-	
-	// Kramer: We need to make two TLS connections - one for the server and one for the client.
+
+	unsigned int Cache = 0;
+	i = 0;
+	//we need to set up a tls. The proxy acts as a middle-man in-between the server and the client.
+	if (tls_accept_socket(tls_ctx, &tls_cctx, clientsd) == -1)
+	errx(1, "Acception by TLS failed %s", tls_error(tls_ctx));
+	else {
+	do {
+	if ((i = tls_handshake(tls_cctx)) == -1)
+	errx(1, "TLS handshake failed %s", tls_error(tls_ctx));
+		} while(i == TLS_WANT_POLLIN || i == TLS_WANT_POLLOUT);
+	}
+	char buffer[200], 
+	//Now it is time to get the object from the client.
+	ssize_t file;
+	file = tls_read(tls_cctx, buffer,200);
+	//Now that we have the file name, it is time to search the bloom filter
+	filter.insert(str_list[i]);
+
+	//for inserting the Bloom values
 	
